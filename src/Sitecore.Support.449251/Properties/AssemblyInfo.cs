d8da2bb9658503c338ebf6f344d4fdb316e58a1c
DK\ALTU <altu@sitecore.net>
2017-04-27 20:28:39 +0300

    Patch 449251 iteration one

---
+++ src/Sitecore.Support.449251/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.449251")]
+[assembly: AssemblyProduct("Sitecore.Support.449251")]
+[assembly: ComVisible(false)]
