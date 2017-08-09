85603da5c4f770cad075edb73bf83dfaa2fc2280
Kirill Volovitskiy <kiv@sitecore.net>
2017-08-09 09:37:31 +0300

    repository name changed

---
+++ src/Sitecore.Support.449251.128620/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.449251.128620")]
+[assembly: AssemblyProduct("Sitecore.Support.449251.128620")]
+[assembly: ComVisible(false)]
