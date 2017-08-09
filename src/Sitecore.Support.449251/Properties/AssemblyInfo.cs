e5e0c3e15d90f446985fffae838039438ab3c53e
Kirill Volovitskiy <kiv@sitecore.net>
2017-08-09 09:39:10 +0300

    Old name removed

--- src/Sitecore.Support.449251/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Sitecore.Support.449251")]
-[assembly: AssemblyProduct("Sitecore.Support.449251")]
-[assembly: ComVisible(false)]
